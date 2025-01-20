2746dd8753fa8638fe9a070202a51aee429cc2af
oht333 <mainse3208@naver.com>
2025-01-20 12:38:30 +0900

    EDIT : 20250120 오현택 StudyDAO, StudyDTO, StudyFrontController, StudyListController 수정

--- src/main/java/com/knowledgeForest/controller/study/StudyFrontController.java
+++ src/main/java/com/knowledgeForest/controller/study/StudyFrontController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.knowledgeForest.Result;
+
 /**
  * Servlet implementation class StudyFrontController
  */
@@ -27,7 +29,26 @@ public class StudyFrontController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("study 서블릿 실행");
+		
+		// request.getContextPath() URL 루트경로를 의미
+		System.out.println(request.getContextPath());
+
+		// request.getRequestURI 현재 요청의 URI를 의미
+		System.out.println(request.getRequestURI());
+		
+		// 요청url이 뭔지 구분하기
+		// 우리가 필요한 것은 전체 URI에서 ContextPath를 제외시킨 부분이다
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println(target);
+
+		Result result = null; //+++++
+
+//		switch (target) {
+//		case "/study
+//		}
+		request.getRequestDispatcher("Study").forward(request, response);
 	}
 
 	/**
--- src/main/java/com/knowledgeForest/dao/StudyDAO.java
+++ src/main/java/com/knowledgeForest/dao/StudyDAO.java
@@ -1,5 +1,22 @@
 package com.knowledgeForest.dao;
 
-public class StudyDAO {
+import org.apache.ibatis.session.SqlSession;
 
+import com.knowledgeForest.config.MyBatisConfig;
+import com.knowledgeForest.dto.StudyDTO;
+
+public class StudyDAO {
+	public SqlSession sqlSession;
+	
+	public StudyDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	public void showList(StudyDTO studyDTO) {
+//		sqlSession.select(null, null);
+	}
+	
+	public void add(StudyDTO studyDTO) {
+		sqlSession.insert("study.list", studyDTO);
+	}
 }
