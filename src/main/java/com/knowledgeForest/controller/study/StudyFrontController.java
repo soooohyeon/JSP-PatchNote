f63f59a929d0f6051fd13075f98b2926c44ee661
unknown <mainse3208@naver.com>
2025-01-20 21:46:33 +0900

    EDIT : 20250120 오현택 클론 중

--- src/main/java/com/knowledgeForest/controller/study/StudyFrontController.java
+++ src/main/java/com/knowledgeForest/controller/study/StudyFrontController.java
@@ -28,27 +28,29 @@ public class StudyFrontController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("study 서블릿 실행");
-		
-		// request.getContextPath() URL 루트경로를 의미
-		System.out.println(request.getContextPath());
+//		// TODO Auto-generated method stub
+////		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		System.out.println("study 서블릿 실행");
+//		
+//		// request.getContextPath() URL 루트경로를 의미
+//		System.out.println(request.getContextPath());
+//
+//		// request.getRequestURI 현재 요청의 URI를 의미
+//		System.out.println(request.getRequestURI());
+//		
+//		// 요청url이 뭔지 구분하기
+//		// 우리가 필요한 것은 전체 URI에서 ContextPath를 제외시킨 부분이다
+//		String target = request.getRequestURI().substring(request.getContextPath().length());
+//		System.out.println(target);
 
-		// request.getRequestURI 현재 요청의 URI를 의미
-		System.out.println(request.getRequestURI());
-		
-		// 요청url이 뭔지 구분하기
-		// 우리가 필요한 것은 전체 URI에서 ContextPath를 제외시킨 부분이다
-		String target = request.getRequestURI().substring(request.getContextPath().length());
-		System.out.println(target);
-
-		Result result = null; //+++++
+//		Result result = null; //+++++
 
 //		switch (target) {
 //		case "/study
 //		}
-		request.getRequestDispatcher("Study").forward(request, response);
+//		request.getRequestDispatcher("Study").forward(request, response);
+		
+		doProcess(request, response);
 	}
 
 	/**
@@ -56,7 +58,33 @@ public class StudyFrontController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("스터디 게시판 컨트롤러 입장");
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		Result result = null;
+		
+		switch(target) {
+		case "/study/studylist.st":
+			request.getRequestDispatcher("/html/study/study/studylist.jsp").forward(request, response);
+			break;
+		case "/study/studyWrite.st":
+			System.out.println("ddddd");
+			request.getRequestDispatcher("/html/study/study/studylist-write.jsp").forward(request, response);
+			break;
+		}
+		
+		if(result != null) {
+			if(result.isRedirect()) {
+				response.sendRedirect(result.getPath());
+			} else {
+				request.getRequestDispatcher(result.getPath()).forward(request, response);
+			}
+		}
+		
 	}
 
 }
