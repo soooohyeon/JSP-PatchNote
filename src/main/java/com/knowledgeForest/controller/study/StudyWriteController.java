0599a4665163271cec6fd0777b65799e3f3316c1
oht333 <mainse3208@naver.com>
2025-01-20 17:28:36 +0900

    스터디게시판 추가기능 적용중

---
+++ src/main/java/com/knowledgeForest/controller/study/StudyWriteController.java
@@ -0,0 +1,36 @@
+package com.knowledgeForest.controller.study;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.knowledgeForest.Execute;
+import com.knowledgeForest.Result;
+import com.knowledgeForest.dao.StudyDAO;
+
+public class StudyWriteController implements Execute {
+
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StudyDAO studyDAO = new StudyDAO();
+		Result result = new Result();
+		HttpSession session = request.getSession();
+		Integer studyNumber = (Integer)session.getAttribute("studyNumber");
+		String path = null;
+		
+		if(studyNumber == null) {
+			path = "/app/main/main-login.html";
+		}else {
+			path = "/app/board/boardWrite.jsp";
+			request.setAttribute(path, session);
+		}
+		
+		result.setPath(path);
+		result.setRedirect(false);
+		return null;
+	}
+
+}
