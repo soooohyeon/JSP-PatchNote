f40c9d50eb10362b5c9b27e66543a551d55f6f82
hh-123--hh <hooj1027@naver.com>
2025-01-21 11:54:09 +0900

    EDIT : 20250121 강후현 중간 커밋

---
+++ src/main/java/com/knowledgeForest/dao/MainDAO.java
@@ -0,0 +1,27 @@
+package com.knowledgeForest.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import com.knowledgeForest.config.MyBatisConfig;
+import com.knowledgeForest.dto.LikeDTO;
+
+import java.util.List;
+
+public class MainDAO {
+
+    private SqlSession sqlsession;
+
+    public MainDAO() {
+        sqlsession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+    }
+
+    // 위시리스트 조회
+    public List<LikeDTO> getWishList(int userNum) {
+        System.out.println("사용자 번호로 위시리스트 조회: " + userNum);
+        List<LikeDTO> wishList = sqlsession.selectList("MainMapper.getWishList", userNum);
+        return wishList;
+        
+    }
+    
+   // 신규 등록 스터디
+
+}
--- src/main/java/com/knowledgeForest/dao/MyPageDAO.java
+++ src/main/java/com/knowledgeForest/dao/MyPageDAO.java
@@ -1,5 +1,38 @@
 package com.knowledgeForest.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.knowledgeForest.config.MyBatisConfig;
+import com.knowledgeForest.dto.UserDTO;
+
 public class MyPageDAO {
+
+   public SqlSession sqlsession;
+
+   public MyPageDAO() {
+      sqlsession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+   }
+
+   // 회원정보 조회
+   public UserDTO getUserInfo(String userId) {
+      userId = "eunji69"; // 수정 필요 ;
+      System.out.println("회원정보" + userId);
+      UserDTO user = sqlsession.selectOne("UserMapper.getUserInfo", userId);
+      return user;
+   }
+
+
+  //회원 정보 수정
+  public int updateUserInfo(UserDTO user) {
+      System.out.println("회원정보 수정 : " + user);
+      int result = 0;
+      result = sqlsession.update("UserMapper.updateUser", user);
+      return result;
+   }
+
+   // 닉네임 중복 체크
+   public boolean checkNickname(String userNick) {
+      return sqlsession.selectOne("UserMapper.checkNickname", userNick);
+   }
 
 }
